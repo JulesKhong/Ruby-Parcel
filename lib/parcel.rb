f156ec03e69fb58703d8020396c32b3cab840deb
Julia Dickey and Stephanie Gurung <julia@>
2016-07-05 12:06:21 -0700

    Creates a spec for flat rate boxes small through large

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,11 +1,12 @@
 class Parcel
-  define_method(:initialize) do |width, height, length, weight, distance, speed|
+  define_method(:initialize) do |width, height, length, weight, distance, speed, wrapping|
     @width = width.to_f()
     @height = height.to_f()
     @length = length.to_f()
     @weight = weight.to_f()
     @distance = distance.to_f()
-    @speed = speed.to_f()
+    @speed = speed
+    @wrapping = wrapping
   end
 
   define_method(:volume) do
@@ -13,17 +14,47 @@ class Parcel
     volume.round(2)
   end
 
+  define_method(:surface) do
+    surface = ((@length.*(@height)).*(2)).+((@length.*(@width)).*(2)).+((@height.*(@width)).*(2))
+    surface.round(2)
+    end
+
   define_method(:cost_to_ship) do
     price = 10.00
-    if self.volume() > 144
-      price = price.+(((self.volume()).-(144)).*(0.10))
+    if @flat == "small"
+      price = 6.80
+      @length = 8.625
+      @width = 5.375
+      @height = 1.625
+    elsif @flat == "medium"
+      price = 13.45
+      @length = 13.625
+      @width = 11.875
+      @height = 3.375
+    elsif @flat == "large"
+      price = 18.75
+      @length = 23.6875
+      @width = 11.75
+      @height = 3
+    else
+      if self.volume() > 144
+        price = price.+(((self.volume()).-(144)).*(0.10))
+      end
+      if @weight > 2
+        price = price.+(@weight-2)
+      end
+      if @distance > 500
+        price = price.+((@distance.-(500)).*(0.01))
+      end
     end
-    if @weight > 2
-      price = price.+(@weight-2)
-    end
-    if @distance > 500
-      price = price.+((@distance.-(500)).*(0.01))
-    end
+      if @speed == "2-day"
+        price = price.+(5)
+      elsif @speed == "overnight"
+        price = price.+(20)
+      end
+      if @wrapping
+        price = price.+(((self.surface())./(1296)).*(5))
+      end
     price.round(2)
   end
 end
