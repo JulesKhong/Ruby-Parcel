50d5eb888f0999cfed465296bbbbd1a541d9ab2c
Julia Dickey and Stephanie Gurung <julia@>
2016-07-05 11:16:07 -0700

    Pass spec returns a price of 4.80 for a package that has a volume of 192 square inches

--- lib/parcel.rb
+++ lib/parcel.rb
@@ -1,12 +1,29 @@
 class Parcel
-  define_method(:initialize) do |width, height, length|
-    @width = width
-    @height = height
-    @length = length
+  define_method(:initialize) do |width, height, length, weight, distance, speed|
+    @width = width.to_f()
+    @height = height.to_f()
+    @length = length.to_f()
+    @weight = weight.to_f()
+    @distance = distance.to_f()
+    @speed = speed.to_f()
   end
 
   define_method(:volume) do
     volume = @length.*(@width).*(@height)
     volume
+  end
+
+  define_method(:cost_to_ship) do
+    price = 10.00
+    if self.volume() > 144
+      price = price.+(((self.volume()).-(144)).*(0.10))
+    end
+    if @weight > 2
+      price = price.+(@weight-2)
+    end
+    if @distance > 500
+      price = price.+((@distance.-(500)).*(0.01))
+    end
+    price.round(2)
   end
 end
--- spec/parcel_spec.rb
+++ spec/parcel_spec.rb
@@ -4,8 +4,20 @@ require('rspec')
 describe(Parcel) do
   describe('#volume') do
     it('returns the volume of a package') do
-      test_package = Parcel.new(2, 3, 4)
+      test_package = Parcel.new(2, 3, 4, 1, 10, 'standard')
       expect(test_package.volume()).to (eq(24))
+    end
+  end
+
+  describe('#cost_to_ship') do
+    it('returns a price of $10 for a package that has a volume less than 144 square inches, a weight less than 2 pounds, a distance less than 500 miles, and standard shipping') do
+      test_package = Parcel.new(2, 3, 4, 1, 10, 'standard')
+      expect(test_package.cost_to_ship()).to (eq(10.00))
+    end
+
+    it('returns a price of $14.80 for a package that has a volume of 192 square inches') do
+      test_package = Parcel.new(4, 4, 12, 1, 10, 'standard')
+      expect(test_package.cost_to_ship()).to (eq(14.80))
     end
   end
 end
